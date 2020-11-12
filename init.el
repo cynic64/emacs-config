6c22e3bcaa1f9d8ed7d9f055821b32e72ff4624e
Nicholas Schweitzer <f3bl@gmx.com>
2020-11-12 07:21:08 +0100

    Temp commit

--- init.el
+++ init.el
@@ -221,8 +221,12 @@ buffer is not visiting a file."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(auth-source-save-behavior nil)
- '(package-selected-packages '(magit hydra emms)))
+ '(custom-safe-themes
+   '("7451f243a18b4b37cabfec57facc01bd1fe28b00e101e488c61e1eed913d9db9" default))
+ '(package-selected-packages '(dracula-theme magit hydra emms)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
