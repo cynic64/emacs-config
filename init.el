6ebddd3c46e37088493f8fdfd03b88b6846ae805
Nicholas Schweitzer <f3bl@gmx.com>
2020-11-12 07:32:21 +0100

    Theme and add elpa/

--- init.el
+++ init.el
@@ -16,6 +16,27 @@ There are two things you can do about this warning:
   )
 (package-initialize)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(auth-source-save-behavior nil)
+ '(custom-safe-themes
+   '("b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "7451f243a18b4b37cabfec57facc01bd1fe28b00e101e488c61e1eed913d9db9" default))
+ '(package-selected-packages '(dracula-theme magit hydra emms)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Theme
+(load-theme 'cyberpunk)
+
 ;; Uses /bin/sh otherwise
 (setq explicit-shell-file-name "/bin/bash")
 
@@ -217,32 +238,14 @@ buffer is not visiting a file."
 ;; Backups go in ~/.emacs.d/backups/
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(auth-source-save-behavior nil)
- '(custom-safe-themes
-   '("7451f243a18b4b37cabfec57facc01bd1fe28b00e101e488c61e1eed913d9db9" default))
- '(package-selected-packages '(dracula-theme magit hydra emms)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; EMMS
 (if (eq system-type 'gnu/linux)
-    ((require 'emms-setup)
-     (emms-all)
-     (emms-default-players)
-     (setq emms-source-file-default-directory "~/msc/collection/")
-     (global-set-key (kbd "M-SPC") 'emms-pause)
-     (emms-add-directory-tree emms-source-file-default-directory)))
+    (progn (require 'emms-setup)
+	   (emms-all)
+	   (emms-default-players)
+	   (setq emms-source-file-default-directory "~/msc/collection/")
+	   (global-set-key (kbd "M-SPC") 'emms-pause)
+	   (emms-add-directory-tree emms-source-file-default-directory)))
 
 (global-set-key (kbd "<XF86AudioStop>") 'emms)
 
