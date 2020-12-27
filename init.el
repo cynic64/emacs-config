9138c5f03fa072244c9f22b3b3298e2dc6723969
Nicholas Schweitzer <f3bl@gmx.com>
2020-12-27 19:52:24 +0100

    Cleanup!

--- init.el
+++ init.el
@@ -1,69 +1,110 @@
-;; MELPA
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-  ;; and `package-pinned-packages`. Most users will not need or want to do this.
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  )
-(package-initialize)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(auth-source-save-behavior nil)
- '(custom-safe-themes
-   '("b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "7451f243a18b4b37cabfec57facc01bd1fe28b00e101e488c61e1eed913d9db9" default))
- '(package-selected-packages '(emms vterm dracula-theme magit hydra)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 ;; Theme
-(load-theme 'cyberpunk)
+(use-package doom-themes
+	     :config
+	     (setq custom-safe-themes '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570"))
+	     (load-theme 'doom-one))
 
-;; Uses /bin/sh otherwise
+;; Evil bindings
+(use-package evil
+  :config
+  (evil-mode 1)
+  (evil-define-key 'normal 'global (kbd "C-w 1") 'delete-other-windows)
+  (evil-define-key 'insert 'global (kbd "C-e") 'end-of-line)
+  (evil-define-key 'insert 'global (kbd "C-a") 'beginning-of-line)
+  (evil-define-key 'normal 'global (kbd "<tab>") 'indent-for-tab-command)
+  (evil-add-hjkl-bindings help-mode-map completion-list-mode-map
+    (kbd "/")       'evil-search-forward
+    (kbd "n")       'evil-search-next
+    (kbd "N")       'evil-search-previous))
+
+(use-package evil-magit)
+
+;; EMMS
+(use-package emms
+  :config
+  (if (eq system-type 'gnu/linux)
+      (progn (require 'emms-setup)
+	     (emms-all)
+	     (setq emms-source-file-default-directory "~/msc/collection/")
+	     (define-emms-simple-player mine '(file) (emms-player-simple-regexp "mp3" "flac")
+	       "mplayer"  "-slave" "-quiet" "-really-quiet" "-novideo")
+	     (emms-player-set emms-player-mine 'pause 'emms-player-mine-pause)
+	     (emms-player-set emms-player-mine 'resume 'emms-player-mine-pause)
+	     (defun emms-player-mine-pause ()
+	       (process-send-string
+		emms-player-simple-process-name "pause\n"))
+	     (setq emms-player-list '(emms-player-mine))
+	     (global-set-key (kbd "<XF86AudioPlay>") 'emms-pause)
+	     (global-set-key (kbd "<XF86AudioStop>") 'emms-browser))))
+
+;; VTerm
+(use-package vterm
+  :config
+  ;; Alt-[0-9] to shells
+  (defun open-shell-as-root ()
+    (interactive)
+    (let ((default-directory "/sudo::/"))
+      (shell "s9")))
+
+  (defun my-term (name)
+    (interactive)
+    (if (get-buffer name)
+	(switch-to-buffer name)
+      (vterm name)))
+
+  ;; Fix C-p being broken in vterm because evil tries to use it for completion
+  (evil-define-key 'insert vterm-mode-map (kbd "C-p") 'vterm--self-insert)
+  (evil-define-key 'insert vterm-mode-map (kbd "C-n") 'vterm--self-insert)
+
+  (setq elp-function-list '(ansi-term-in-default-dir))
+
+  (global-set-key (kbd "C-0") (lambda () (interactive) (my-term "s0")))
+  (global-set-key (kbd "C-1") (lambda () (interactive) (my-term "s1")))
+  (global-set-key (kbd "C-2") (lambda () (interactive) (my-term "s2")))
+  (global-set-key (kbd "C-3") (lambda () (interactive) (my-term "s3")))
+  (global-set-key (kbd "C-4") (lambda () (interactive) (my-term "s4")))
+  (global-set-key (kbd "C-5") (lambda () (interactive) (my-term "s5")))
+  (global-set-key (kbd "C-6") (lambda () (interactive) (my-term "s6")))
+  (global-set-key (kbd "C-7") (lambda () (interactive) (my-term "s7")))
+  (global-set-key (kbd "C-8") (lambda () (interactive) (my-term "s8")))
+  (global-set-key (kbd "C-9") 'open-shell-as-root)
+
+  ;; Meta doesn't work on mac for whatever reason
+  (if (eq system-type 'darwin)
+      (progn (global-set-key (kbd "s-0") (lambda () (interactive) (my-term "s0")))
+	     (global-set-key (kbd "s-1") (lambda () (interactive) (my-term "s1")))
+	     (global-set-key (kbd "s-2") (lambda () (interactive) (my-term "s2")))
+	     (global-set-key (kbd "s-3") (lambda () (interactive) (my-term "s3")))
+	     (global-set-key (kbd "s-4") (lambda () (interactive) (my-term "s4")))
+	     (global-set-key (kbd "s-5") (lambda () (interactive) (my-term "s5")))
+	     (global-set-key (kbd "s-6") (lambda () (interactive) (my-term "s6")))
+	     (global-set-key (kbd "s-7") (lambda () (interactive) (my-term "s7")))
+	     (global-set-key (kbd "s-8") (lambda () (interactive) (my-term "s8")))
+	     (global-set-key (kbd "s-9") 'open-shell-as-root)))
+
+  (setq same-window-regexps '("s[[:digit:]]")))
+
+(use-package magit)
+
+;; Shell-related stuff
 (setq explicit-shell-file-name "/bin/bash")
-
-;; Global agenda
-(setq org-agenda-files (list "~/org/school.org"
-			     "~/org/me.org"))
-
-;; Map C-x C-a to open agenda
-(global-set-key (kbd "C-x C-a") 'org-agenda-list)
-;; Map C-x t to open todo list
-(global-set-key (kbd "C-x t") 'org-todo-list)
-
-;; Add CLOSED timestamp each time a TODO is completed
-(setq org-log-done 'time)
-;; Save clock history across sessions
-(setq org-clock-persist 'history)
-(org-clock-persistence-insinuate)
-
-;; Always show 7 days into the future
-(setq org-agenda-start-on-weekday nil)
-
-;; Capture
-(setq org-default-notes-file "~/org/notes.org")
-
-(defun org-capture-todo () (interactive) (org-capture nil "t"))
-(defun org-capture-todo-exclusive () (interactive) (org-capture-todo) (delete-other-windows))
-(global-set-key (kbd "M-g M-r") 'org-capture-todo)
+(setq shell-file-name "/bin/bash")
 
 ;; Wrapping
 (add-hook 'text-mode-hook 'auto-fill-mode)
@@ -77,19 +118,8 @@ There are two things you can do about this warning:
   (column-number-mode))
 (add-hook 'c-mode-common-hook 'my-c-mode-hook)
 
-;; EMMS
-(if (eq system-type 'gnu/linux)
-    (progn (require 'emms-setup)
-	   (emms-all)
-	   (setq emms-source-file-default-directory "~/msc/collection/")
-	   (define-emms-simple-player play '(file) (emms-player-simple-regexp "mp3" "flac")
-	     "mplayer"  "-slave" "-quiet" "-really-quiet" "-novideo")
-	   (setq emms-player-list '(emms-player-play))))
-
-;; M-g M-p pipes region through perl -wle <minibuffer input>
-(setq shell-file-name "/bin/bash")
-
-(defun my-pipe-selection (command arg)
+;; Pipes region through perl -wle <minibuffer input>
+(defun perl-pipe-selection (command arg)
   (interactive (list (read-from-minibuffer "perl -wle: " nil nil nil 'shell-command-history)
 		     current-prefix-arg))
   (let ((real-command (concat "perl -wle '" command "'")))
@@ -105,181 +135,32 @@ There are two things you can do about this warning:
 	    (shell-command-on-region p m real-command t t)
 	  (shell-command-on-region p m real-command))))))
 
-(global-set-key (kbd "M-g M-p") 'my-pipe-selection)
-
-;; Map M-g M-e to eval-buffer
-(global-set-key (kbd "M-g M-e") 'eval-buffer)
-
-;; M-g M-i to imenu
-(global-set-key (kbd "M-g M-i") 'imenu)
-(put 'downcase-region 'disabled nil)
-
-;; Reopen as root
-(defun er-sudo-edit (&optional arg)
-  "Edit currently visited file as root.
-
-With a prefix ARG prompt for a file to visit.
-Will also prompt for a file to visit if current
-buffer is not visiting a file."
-  (interactive "P")
-  (if (or arg (not buffer-file-name))
-      (find-file (concat "/sudo:root@localhost:"
-                         (ido-read-file-name "Find file(as root): ")))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
-(global-set-key (kbd "C-x C-r") #'er-sudo-edit)
-(put 'narrow-to-region 'disabled nil)
-
-;; Zap until
-(global-set-key (kbd "C-z") 'zap-up-to-char)
-
-;; Yank line
-(defun yank-line ()
-  "Yanks current line in its entirety."
-  (interactive)
-  (save-excursion
-    (move-beginning-of-line 1)
-    (let ((beg (point)))
-      (forward-line 1)
-      (copy-region-as-kill beg (point)))))
-
-(defun duplicate-line ()
-  "Duplicate current line."
-  (interactive)
-  (save-excursion
-    (yank-line)
-    (move-beginning-of-line 1)
-    (yank)))
-
-;; Yank paragraph
-(defun yank-paragraph ()
-  "Yanks current paragraph in its entirety."
-  (interactive)
-  (save-excursion
-    (backward-paragraph)
-    (let ((beg (point)))
-      (forward-paragraph)
-      (copy-region-as-kill beg (point)))))
-
-;; Format function definition/call
-(defun format-fun ()
-  "Format function definition/call."
-  (interactive)
-  (save-excursion
-    (down-list)
-    (merge-lines)
-    (backward-up-list)
-    (forward-list)
-    (previous-line 1)
-    (move-end-of-line 1)
-    (merge-lines)
-    (backward-up-list)
-    (let ((beg (point)))
-      (forward-list)
-      (indent-region beg (point)))))
-
-;; Merges all [ \t\n] after cursor
-(defun merge-lines ()
-  (let ((beg (point)))
-    (skip-chars-forward " \t\n")
-    (delete-region beg (point))))
-
-;; [C] Yank statement
-(defun yank-statement ()
-  "[C] Yank current statement in its entirety."
-  (interactive)
-  (save-excursion
-    (c-beginning-of-statement-1)
-    (let ((beg (point)))
-      (c-end-of-statement)
-      (copy-region-as-kill beg (point)))))
-
 ;; No GUI crap
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; Alt-[0-9] to shells
-(defun open-shell-as-root ()
-  (interactive)
-  (let ((default-directory "/sudo::/"))
-    (shell "s9")))
-
-(defun my-term (name)
-  (interactive)
-  (if (get-buffer name)
-      (switch-to-buffer name)
-    (vterm name)))
-
-(setq elp-function-list '(ansi-term-in-default-dir))
-
-(global-set-key (kbd "C-0") (lambda () (interactive) (my-term "s0")))
-(global-set-key (kbd "C-1") (lambda () (interactive) (my-term "s1")))
-(global-set-key (kbd "C-2") (lambda () (interactive) (my-term "s2")))
-(global-set-key (kbd "C-3") (lambda () (interactive) (my-term "s3")))
-(global-set-key (kbd "C-4") (lambda () (interactive) (my-term "s4")))
-(global-set-key (kbd "C-5") (lambda () (interactive) (my-term "s5")))
-(global-set-key (kbd "C-6") (lambda () (interactive) (my-term "s6")))
-(global-set-key (kbd "C-7") (lambda () (interactive) (my-term "s7")))
-(global-set-key (kbd "C-8") (lambda () (interactive) (my-term "s8")))
-(global-set-key (kbd "C-9") 'open-shell-as-root)
-
-;; Meta doesn't work on mac for whatever reason
-(if (eq system-type 'darwin)
-    (progn (global-set-key (kbd "s-0") (lambda () (interactive) (my-term "s0")))
-	   (global-set-key (kbd "s-1") (lambda () (interactive) (my-term "s1")))
-	   (global-set-key (kbd "s-2") (lambda () (interactive) (my-term "s2")))
-	   (global-set-key (kbd "s-3") (lambda () (interactive) (my-term "s3")))
-	   (global-set-key (kbd "s-4") (lambda () (interactive) (my-term "s4")))
-	   (global-set-key (kbd "s-5") (lambda () (interactive) (my-term "s5")))
-	   (global-set-key (kbd "s-6") (lambda () (interactive) (my-term "s6")))
-	   (global-set-key (kbd "s-7") (lambda () (interactive) (my-term "s7")))
-	   (global-set-key (kbd "s-8") (lambda () (interactive) (my-term "s8")))
-	   (global-set-key (kbd "s-9") 'open-shell-as-root)))
-
-;; Make C-x behave normally in term mode
-(add-hook 'term-mode-hook (lambda ()
-                            (let (term-escape-char)
-                              (term-set-escape-char ?\C-x))
-                            (let (term-escape-char)
-                              (term-set-escape-char ?\C-c))))
-
-(setq same-window-regexps '("s[[:digit:]]"))
-
 ;; Cache TRAMP passwords forever
 (setq password-cache-expiry nil)
 
-;; Recent files
-(recentf-mode 1)
-(setq recentf-max-menu-items 256)
-(setq recentf-max-saved-items 256)
-(global-set-key (kbd "C-x C-y") 'recentf-open-files)
-;; Save every 5 mins
-(run-at-time nil (* 5 60) 'recentf-save-list)
-;; Map to C-x f
-(global-set-key (kbd "C-x f") 'recentf-open-files)
+;; Better buffer switching
+(setq pop-up-frames 'graphic-only)
+(setq ido-default-buffer-method 'selected-window)
 
-;; Backups go in ~/.emacs.d/backups/
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
+(use-package general :ensure t
+	     :config
+	     (general-define-key
+	      :keymaps '(normal visual emacs)
+	      :prefix "SPC"
+	      :non-normal-prefix "M-m"
+	      ;; Project
+	      "p m" '(magit-status :which-key "magit")
+	      ;; Buffer
+	      "b b" '(switch-to-buffer :which-key "switch")
+	      ;; Execute
+	      "x e" '(eval-buffer :which-key "eval buffer")))
 
-;; Ido
-(ido-mode 1)
-
-;; Hydras
-(global-set-key
- (kbd "M-m")
- (defhydra hydra-main (:exit t)
-   "My stuff"
-   ("y" yank-line "Yank line")
-   (")" blink-matching-open "Show matching (")
-   ("p" yank-paragraph "Yank paragraph")
-   ("s" yank-statement "[C] Yank statement")
-   ("f" format-fun "Format function definition/call")
-   ("j" join-line "Join line")
-   ("d" duplicate-line "Duplicate line")
-   ("M-m" back-to-indentation "Back to indentation")
-   ("<RET>" make-frame-command "New frame")))
-(put 'set-goal-column 'disabled nil)
-
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
+(use-package which-key
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.3))
